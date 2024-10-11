d6c1e34163045ea1324f0cc28cc9b0cbc4bf4f87
DaviAzevedo <daviazevedos02@gmail.com>
2024-10-11 17:06:40 -0300

    Add api do kaggle

---
+++ KaggleApi/import_data.py
@@ -0,0 +1,9 @@
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("valakhorasani/mobile-device-usage-and-user-behavior-dataset")
+
+print("Path to dataset files:", path)
+
+
+
