175cd87988b548d33ba11df81dc8f4b176b0b392
DaviAzevedo <daviazevedos02@gmail.com>
2024-10-11 20:12:18 -0300

    Novo main

--- src/main.py
+++ src/main.py
@@ -1,30 +1,36 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from utils.data_loader import load_data
-from utils.treat_path import treat_path
-from mappers.semantic_mapper import infer_mapping
-from ontology.ontology_generator import generate_ontology
-from sklearn.preprocessing import LabelEncoder
-from sklearn.cluster import KMeans
+from app import OntologyConverterApp
+import tkinter as tk
 
-path = 'data/games.csv'
-apath = 'data/user_behavior_dataset.csv'
-apath = 'data/gym_members_exercise_tracking.csv'
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = OntologyConverterApp(root)
+    root.mainloop()
+
+# import pandas as pd
+# import numpy as np
+# import matplotlib.pyplot as plt
+# from utils.data_loader import load_data
+# from utils.treat_path import treat_path
+# from mappers.semantic_mapper import infer_mapping
+# from ontology.ontology_generator import generate_ontology
+# from sklearn.preprocessing import LabelEncoder
+# from sklearn.cluster import KMeans
+
+# path = 'data/games.csv'
 
 
-def pipeline(path, table_name=None):
-    name, source_type = treat_path(path)
+# def pipeline(path, table_name=None):
+#     name, source_type = treat_path(path)
 
-    # Carregar os dados
-    data = load_data(source_type, path, table_name)
+#     Carregar os dados
+#     data = load_data(source_type, path, table_name)
     
-    # Inferir mapeamentos semânticos
-    mappings = infer_mapping(data.sample(n=10))
+#     Inferir mapeamentos semânticos
+#     mappings = infer_mapping(data)
     
-    # Gerar ontologia OWL
-    generate_ontology(data, mappings, name)
+#     Gerar ontologia OWL
+#     generate_ontology(data, mappings, name)
     
-    print("Ontologia gerada com sucesso!")
+#     print("Ontologia gerada com sucesso!")
 
-pipeline(path)
+# pipeline(path)
