090a0303255e959ebee9fd4915937e0b3efaf0f0
DaviAzevedo <daviazevedos02@gmail.com>
2024-10-11 17:09:39 -0300

    Limpeza e pequenas modificações

--- src/main.py
+++ src/main.py
@@ -2,32 +2,27 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from utils.data_loader import load_data
+from utils.treat_path import treat_path
 from mappers.semantic_mapper import infer_mapping
 from ontology.ontology_generator import generate_ontology
 from sklearn.preprocessing import LabelEncoder
 from sklearn.cluster import KMeans
 
-sourceType = 'csv'
-sourcePath = 'data/MentalHealthSurvey.csv'
-# sourcePath = 'data/social-media.csv'
+path = 'data/games.csv'
 
-def pipeline(source_type, source_path):
+
+def pipeline(path, table_name=None):
+    name, source_type = treat_path(path)
+
     # Carregar os dados
-    data = load_data(source_type, source_path)
-
-    # print(data.dtypes)
-    # transposed_data = data.T
-    # print(transposed_data.dtypes)
-    # column_features = transposed_data.apply(LabelEncoder().fit_transform).T
-    # print(column_features.dtypes)
-
+    data = load_data(source_type, path, table_name)
     
     # Inferir mapeamentos semânticos
     mappings = infer_mapping(data)
     
     # Gerar ontologia OWL
-    generate_ontology(data, mappings)
+    generate_ontology(data, mappings, name)
     
     print("Ontologia gerada com sucesso!")
 
-pipeline(sourceType, sourcePath)
+pipeline(path)
